460aba5177abf86a2de31c766d9852db2b36d7ca
leozin777 <leo.cardoso@rede.ulbra.br>
2023-03-28 20:36:23 -0300

    chamei na main

--- Seminario-28-03/ex1/src/Main.java
+++ Seminario-28-03/ex1/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Relogio relogio = new Relogio();
+
+        relogio.acertarRelogio(2,30,55);
+        System.out.println(relogio.getHora());
+        System.out.println(relogio.getMinuto());
+        System.out.println(relogio.getSegundo());
     }
 }
