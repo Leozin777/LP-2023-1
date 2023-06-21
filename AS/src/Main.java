f31aa46c7a1d002633559e2bc1cf7fb67c945f43
leozin777 <leo.cardoso@rede.ulbra.br>
2023-06-20 22:34:17 -0300

    as feita

--- AS/src/Main.java
+++ AS/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+        Ave beto = new Ave();
+        beto.setEspecie("Periquito");
+        beto.setIdade(1);
+        beto.setNome("beto");
+        beto.setPeso(0.3);
+        beto.setEnvergaduraAsa(0.18);
 
+        beto.Movimentar();
+        beto.Movimentar("voar");
+
+        beto.RealizarTruque();
+        beto.Voar();
+
+        Veterinario maria = new Veterinario();
+        maria.setNome("maria");
+        maria.setCargo("Veterinaria");
+        maria.setSalario(4000);
+        maria.setEspecialidade("aves");
+
+        Funcionario marcos = new Funcionario();
+        marcos.setNome("marcos");
+        marcos.setSalario(2500);
+        marcos.setCargo("faxineiro");
+
+        Zoo zoo = new Zoo(1,2);
+
+        zoo.SetAnimal(beto);
+        zoo.SetFuncionario(marcos);
+        zoo.SetFuncionario(maria);
+
+        zoo.getAnimais();
+        zoo.getFuncionarios();
     }
 }
--- AS/src/Veterinario.java
+++ AS/src/Veterinario.java
@@ -2,7 +2,15 @@ public class Veterinario extends Funcionario
 {
     private String Especialidade;
 
-    private void RealizarExame(Animal p_animal)
+    public String getEspecialidade() {
+        return Especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        Especialidade = especialidade;
+    }
+
+    public void RealizarExame(Animal p_animal)
     {
         System.out.println("Realizando exame em um " + p_animal.getEspecie() + " que se chama " + p_animal.getNome());
     }
